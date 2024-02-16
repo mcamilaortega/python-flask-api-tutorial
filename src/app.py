7f6b691baf6410d2adfb596d49344039bbef83fe
Maria Camila Ortega <148597026+mcamilaortega@users.noreply.github.com>
2024-02-16 20:09:19 +0000

    working without learnpack

--- src/app.py
+++ src/app.py
@@ -12,11 +12,27 @@ def hello_world():
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
-    request_body = request.json
+    request_body = request.json 
+    if request_body is None:
+        return "Please send valid todo", 400
     print("Incoming request with the following body", request_body) 
     todos.append(request_body)
     return jsonify(todos)
 
+
+@app.route('/todos/<int:index>', methods=['DELETE'])
+def delete_todo(index):
+    request_body = request.json 
+    if request_body is None:
+        return "Please send valid todo", 400
+    counter=0
+    for todo in todos: 
+        if counter == index:
+            todos.remove(todo)
+        counter=counter+1
+    print("Incoming request with the following body", request_body) 
+
+    return jsonify(todos)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
